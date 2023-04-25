11b6a6e518a3eb9abfdbec5d9c9cc5686d8b31c7
creativdex <creativdex@yandex.ru>
2023-04-25 10:28:58 +0700

    Add - CORS

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 import uvicorn
@@ -14,6 +15,14 @@ logging.getLogger("uvicron").setLevel(logging.WARNING)
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8070)
